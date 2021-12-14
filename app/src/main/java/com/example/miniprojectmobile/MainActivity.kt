36d4093a7abf0aa14c871baaf576b02a2ce12411
Hijir Ismail <hijirismail007@gmail.com>
2021-12-14 16:05:10 +0800

    Update1

--- app/src/main/java/com/example/miniprojectmobile/MainActivity.kt
+++ app/src/main/java/com/example/miniprojectmobile/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.miniprojectmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.miniprojectmobile.userinterface.home.HomeFragment
+import com.example.miniprojectmobile.selected.SelectedKuliner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment: Fragment = HomeFragment.newInstance()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container_fragment, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/example/miniprojectmobile/Network/ApiEndPoint.kt
+++ app/src/main/java/com/example/miniprojectmobile/Network/ApiEndPoint.kt
@@ -1,2 +1,13 @@
 package com.example.miniprojectmobile.Network
 
+import id.ac.unhas.winfood.DataKuliner
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface api {
+
+    @GET("api/purwakarta/kuliner")
+
+    fun getDataKuliner(): Call<DataKuliner>
+}
